b18a1935d2e8d6a1b0839f089973d003f576f9e4
alsusaifetdinova <Alsu102@yahoo.com>
2018-10-21 03:24:09 -0400

    -created manager's home page, will work on design later

---
+++ ProjectResort/src/sample/Controller.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+
+  public Button buttonCompleted;
+  public CheckBox reqest1;
+
+  public void welcomeToFgcuCliked(MouseEvent mouseEvent) {
+
+    //this would take us to home page
+
+
+
+  }
+
+  public void viewRequest1(ActionEvent actionEvent) {
+
+
+    //I dont know if we can implement window-expand on mouse click
+  }
+
+  public void requestCompleted(ActionEvent actionEvent) {
+    //completed button for requests
+  }
+}
---
+++ ProjectResort/src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+  public static void main(String[] args) {
+    launch(args);
+
+
+  }
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Project Resort - FGCU Inn");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+
+}
